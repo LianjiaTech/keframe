3de10d43fa59081c7c6a352b591ebe19a11bb406
nayuta <dujunda@bytedance.com>
2022-05-14 16:47:47 +0800

    adapter embedding2

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -6,7 +6,4 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
